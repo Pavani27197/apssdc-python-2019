e1f65979341255fc7fb2f12626bdd9c260c0e107
pavani.karra27@gmail.com <pavani.karra27@gmail.com>
2019-06-18 18:53:18 +0530

    add files

---
+++ packages/numerical.py
@@ -0,0 +1,21 @@
+#function to check if number is prime
+def prime(n):
+    flag = 1
+    if n==2:
+        return True
+    for i in range(2,n//2 +1):
+        if n%i==0:
+            flag = 0
+            return False
+    if flag == 1:
+        return True
+        
+#function to determine number of prime factors for a given number
+def numberofprimefactors(n):
+    if prime(n):
+        return 1
+    count=0
+    for i in range(2,n//2+1):
+        if prime(i) and n%i==0:
+            count +=1
+    return count
